6cc298d661bd12063ba724630e557c6c5b79c090
Patipol Wuthiprasert <abcteapatipol@gmail.com>
2020-07-07 01:45:56 +0700

    Updates

---
+++ precache-manifest.ffa5b487985bcf26e998dbb1da6d35a5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "056caea691b845eb6acf72aa739a72fb",
+    "url": "/list-search-app/index.html"
+  },
+  {
+    "revision": "b2011de54fe4f77da290",
+    "url": "/list-search-app/static/css/2.c27a359e.chunk.css"
+  },
+  {
+    "revision": "b13fccc5d2d667f7dd18",
+    "url": "/list-search-app/static/css/main.553f9731.chunk.css"
+  },
+  {
+    "revision": "b2011de54fe4f77da290",
+    "url": "/list-search-app/static/js/2.330e918d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/list-search-app/static/js/2.330e918d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b13fccc5d2d667f7dd18",
+    "url": "/list-search-app/static/js/main.908944e6.chunk.js"
+  },
+  {
+    "revision": "82f45956244e47800b35",
+    "url": "/list-search-app/static/js/runtime-main.27628574.js"
+  }
+]);
